6fa7538fa1aedf6559eea46eca22f8ba5bc989ff
khj68 <maga40@naver.com>
2020-10-25 19:45:18 +0900

    Make spring boot project

---
+++ pastel/src/main/java/yapp/pastel/PastelApplication.java
@@ -0,0 +1,13 @@
+package yapp.pastel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PastelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PastelApplication.class, args);
+	}
+
+}

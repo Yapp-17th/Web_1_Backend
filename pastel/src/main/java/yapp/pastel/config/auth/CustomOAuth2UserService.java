7dfbe8504231d0988e3a40a397d0ec82441c5a92
KHJ <maga40@naver.com>
2020-11-09 21:56:29 +0900

    Add token issue

--- pastel/src/main/java/yapp/pastel/config/auth/CustomOAuth2UserService.java
+++ pastel/src/main/java/yapp/pastel/config/auth/CustomOAuth2UserService.java
@@ -4,6 +4,7 @@ import java.util.Collections;
 
 import javax.servlet.http.HttpSession;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
@@ -14,6 +15,8 @@ import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
 
 import lombok.RequiredArgsConstructor;
+import yapp.pastel.config.token.TokenRegistry;
+import yapp.pastel.config.token.TokenUtils;
 import yapp.pastel.domain.user.User;
 import yapp.pastel.repository.UserRepository;
 
@@ -23,27 +26,44 @@ public class CustomOAuth2UserService implements OAuth2UserService {
     private final UserRepository userRepository;
     private final HttpSession httpSession;
 
+    @Autowired private TokenUtils tokenUtils;
+    @Autowired private TokenRegistry tokenRegistry;
+
     @Override
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
         OAuth2UserService delegate = new DefaultOAuth2UserService();
         OAuth2User oAuth2User = delegate.loadUser(userRequest);
+        
+        System.out.println(oAuth2User.toString());
 
         String registrationId = userRequest.getClientRegistration().getRegistrationId();
         String userNameAttributeName = userRequest.getClientRegistration()
                 .getProviderDetails().getUserInfoEndpoint().getUserNameAttributeName();
+
+        System.out.println(registrationId);
+
         
         OAuthAttributes attributes = OAuthAttributes
-                .of(registrationId, userNameAttributeName, oAuth2User.getAttributes());
-    
+        .of(registrationId, userNameAttributeName, oAuth2User.getAttributes());
+        
+        
         User user = saveOrUpdate(attributes);
         httpSession.setAttribute("user", new SessionUser(user));
-
+        
+        //
+        String refreshToken = tokenUtils.createRefreshToken(user);
+        String accessToken = tokenUtils.createAccessToken(user);
+        System.out.println(refreshToken);
+        System.out.println(accessToken);
+        // System.out.println(tokenUtils.getEmail(accessToken));
+        // System.out.println(tokenUtils.getRoles(accessToken));
+        //
         return new DefaultOAuth2User(
             Collections.singleton(new SimpleGrantedAuthority(user.getRoleKey())),
             attributes.getAttributes(),
             attributes.getNameAttributeKey());
     }
-
+    
     private User saveOrUpdate(OAuthAttributes attributes) {
         User user = userRepository.findByEmail(attributes.getEmail())
                 .map(entity -> entity.update(attributes.getName(), attributes.getPicture()))

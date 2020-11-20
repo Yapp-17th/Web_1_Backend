d8e54c245b80c2384802dbc165f6e2c3b1151297
KHJ <maga40@naver.com>
2020-11-20 13:50:04 +0900

    Create tag entity

---
+++ pastel/src/main/java/yapp/pastel/domain/Tag.java
@@ -0,0 +1,19 @@
+package yapp.pastel.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class Tag {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+  private String content;
+}

6c28e6b037ff4db9ee0bd4f12a98faf9dfb6a26d
KHJ <maga40@naver.com>
2020-11-20 13:51:37 +0900

    Create user service

---
+++ pastel/src/main/java/yapp/pastel/service/UserService.java
@@ -0,0 +1,10 @@
+package yapp.pastel.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import yapp.pastel.repository.UserRepository;
+
+public class UserService {
+  @Autowired
+  private UserRepository userRepository;
+}

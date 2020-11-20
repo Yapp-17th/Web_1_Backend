53faeadf2c4bfa112829e99131faf9886ee40579
KHJ <maga40@naver.com>
2020-11-20 13:49:57 +0900

    Create store entity

---
+++ pastel/src/main/java/yapp/pastel/domain/Store.java
@@ -0,0 +1,37 @@
+package yapp.pastel.domain;
+
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class Store {
+  @Id
+  @GeneratedValue( strategy = GenerationType.IDENTITY)
+  private Long id;
+  private String email;
+  private String name;
+  private String photoUrl;
+
+  private String zone;
+  private String location;
+  private String locationDetail;
+  private LocalTime openTime;
+  private LocalTime closeTime;
+  private String notice;
+  private String contact;
+  private String introduce;
+
+  // private List<Cake> cakes = new ArrayList<>();
+}
